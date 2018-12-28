3ecca8781b65a715107c573c0c5764c1abddd149
douglas-m-harvey <46091998+douglas-m-harvey@users.noreply.github.com>
2018-12-28 19:14:20 +0000

    Updated 'room' file reading system, moved room .txt files to a new 'rooms' folder

--- Text-Adventure.py
+++ Text-Adventure.py
@@ -1,16 +1,21 @@
 "Text Adventure"
 
-file = open("room_1.txt")
-room_1 = {}
-for line in file:
-    room_1[line.split(": ")[0]] = line.split(": ")[1]
-file.close()
-    
-#    d['mynewkey'] = 'mynewvalue'
+import os
 
-#file = open("room_1.txt")    
-#for i, line in enumerate(file, 1):
-#    if i % 2 == 0:
-#        print(i)
-#        print(line)
+no_rooms = len(os.listdir("./rooms"))
+list_rooms = [[] for i in range(no_rooms)]
 
+i = -1
+for room in os.listdir("./rooms"):
+    i += 1
+    print(room)
+    os.chdir("./rooms")
+    dictionary = {}
+    file = open(room)
+    for line in file:
+        print(line)
+        dictionary[line.split(": ")[0]] = line.split(": ")[1]
+        print(line.split(": "))
+    list_rooms[i] = dictionary
+    file.close()
+    os.chdir("..")
