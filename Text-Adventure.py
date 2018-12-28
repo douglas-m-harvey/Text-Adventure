bb93eef2148da7479d2e4251441979cc5185a2ac
douglas-m-harvey <46091998+douglas-m-harvey@users.noreply.github.com>
2018-12-28 21:50:38 +0000

    Updated Text-Adventure.py
    Tidied up txt file reading system, added comments

--- Text-Adventure.py
+++ Text-Adventure.py
@@ -1,21 +1,31 @@
 "Text Adventure"
 
+"""
+Import required modules. 
+"""
 import os
 
+"""
+Read the number of room_x.txt files in the rooms directory and make a list of 
+empty lists of this length.
+"""
 no_rooms = len(os.listdir("./rooms"))
 list_rooms = [[] for i in range(no_rooms)]
 
+"""
+For each room_x.txt file in the rooms directory, write its contents to a 
+dictionary and add each dictionary to list_rooms.
+"""
 i = -1
 for room in os.listdir("./rooms"):
     i += 1
-    print(room)
     os.chdir("./rooms")
-    dictionary = {}
+    list_rooms[i] = {}
     file = open(room)
     for line in file:
-        print(line)
-        dictionary[line.split(": ")[0]] = line.split(": ")[1]
-        print(line.split(": "))
-    list_rooms[i] = dictionary
+        list_rooms[i][line.split(": ")[0]] \
+        = \
+        str.strip(line.split(": ")[1])
     file.close()
     os.chdir("..")
+
