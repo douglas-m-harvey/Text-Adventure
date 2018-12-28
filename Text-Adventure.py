650e2941c64e09f87d7bca3565e569c62ff38e8f
douglas-m-harvey <46091998+douglas-m-harvey@users.noreply.github.com>
2018-12-28 22:55:49 +0000

    Changed comment format

--- Text-Adventure.py
+++ Text-Adventure.py
@@ -1,21 +1,15 @@
 "Text Adventure"
 
-"""
-Import required modules. 
-"""
+#Import required modules. 
 import os
 
-"""
-Read the number of room_x.txt files in the rooms directory and make a list of 
-empty lists of this length.
-"""
+#Read the number of room_x.txt files in the rooms directory and make a list of 
+#empty lists of this length.
 no_rooms = len(os.listdir("./rooms"))
 list_rooms = [[] for i in range(no_rooms)]
 
-"""
-For each room_x.txt file in the rooms directory, write its contents to a 
-dictionary and add each dictionary to list_rooms.
-"""
+#For each room_x.txt file in the rooms directory, write its contents to a 
+#dictionary and add each dictionary to list_rooms.
 i = -1
 for room in os.listdir("./rooms"):
     i += 1
@@ -28,4 +22,3 @@ for room in os.listdir("./rooms"):
         str.strip(line.split(": ")[1])
     file.close()
     os.chdir("..")
-
